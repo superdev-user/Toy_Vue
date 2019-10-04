3a2557ce1eb18ca518460eb605a3290e1b7d7fe3
sjeongchoi <tandri6@gmail.com>
2019-10-04 20:57:37 +0900

    make chained select

--- src/components/studySpace/Add/addStudySpace.component.js
+++ src/components/studySpace/Add/addStudySpace.component.js
@@ -13,10 +13,117 @@ export default {
     return {
       title : '',
       description : '',
+      category1: [
+        {
+          label: 'history',
+          id: '1',
+          parent_id: '',
+        } ,
+        {
+          label: 'todo',
+          id: '2',
+          parent_id: '',
+        }
+      ],
+      category2: [
+        {
+          label: '첫번째 스터디',
+          id: '1',
+          parent_id: '1',
+        } ,
+        {
+          label: '두번째 스터디',
+          id: '2',
+          parent_id: '1',
+        },
+        {
+          label: '세번째 스터디',
+          id: '3',
+          parent_id: '1',
+        } ,
+        {
+          label: '네번째 스터디',
+          id: '4',
+          parent_id: '1',
+        },
+        {
+          label: '첫번째 고려사항',
+          id: '5',
+          parent_id: '2',
+        } ,
+        {
+          label: '두번째 해야할 일',
+          id: '6',
+          parent_id: '2',
+        }
+      ],
+      category3: [
+        {
+          label: '주제 선정',
+          id: '1',
+          parent_id: '1',
+        } ,
+        {
+          label: '서비스 구조',
+          id: '2',
+          parent_id: '1',
+        },
+        {
+          label: '환경',
+          id: '3',
+          parent_id: '3',
+        } ,
+        {
+          label: '알고리즘',
+          id: '4',
+          parent_id: '4',
+        },
+        {
+          label: 'test',
+          id: '5',
+          parent_id: '5',
+        } ,
+        {
+          label: 'test coverage',
+          id: '6',
+          parent_id: '6',
+        }
+      ],
+      selectedGrandParent: -1,
+      selectedParent: -1,
+      selectedChild: -1,
     }
   },
-  computed: {},
+  computed: {
+    getGrandParent() {
+      return this.category1
+    },
+    getParent() {
+      let returnVal = this.category2.filter(parent => parent.parent_id == this.selectedGrandParent)
+      return returnVal
+    },
+    getChild() {
+      let returnVal = this.category3.filter(child => child.parent_id == this.selectedParent)
+      return returnVal
+    },
+  },
   methods: {
+    loadData:function() {
+      this.items = [];
+      let key = 'name';
+      if(this.selectedOption === 'films') key = 'title';
+
+      fetch('https://swapi.co/api/'+this.selectedOption)
+          .then(res=>res.json())
+          .then(res => {
+            // "fix" the data to set a label for all types
+            this.items = res.results.map((item) =>{
+              item.label = item[key];
+              return item;
+            });
+
+          });
+    },
     onSubmit(e) {
       e.preventDefault();
       let {userId: writer} = JSON.parse(localStorage.userAccessToken);
